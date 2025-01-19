175419e7176092a2be46d69c435fbe19a83d9ec6
ArmoredClavio <100507854+Jhasper1@users.noreply.github.com>
2025-01-19 18:57:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def send_message(recipient_id, message_text):
     print(f"Message sent: {response.status_code}")
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=10000)
+    app.run(debug=True, host='0.0.0.0', port=3000)
